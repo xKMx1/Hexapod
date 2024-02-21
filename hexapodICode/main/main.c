986adec808a73184441a7f1067d69dca81715190
Kamil Ratajczyk <kamilratajczyk00@gmail.com>
2024-02-21 20:46:03 +0100

    Add new leg part

--- hexapodICode/main/main.c
+++ hexapodICode/main/main.c
@@ -5,13 +5,12 @@ AX_conf_t conf;
 
 void app_main(void)
 {
-    conf.uart = UART_NUM_2;
+    conf.uart = UART_NUM_1;
     conf.tx_pin = 17;
-    conf.rx_pin = 16;
-    conf.rts_pin = 34;  // TX_EN
+    conf.rx_pin = 18;
+    conf.rts_pin = 34;      // TX_EN
     conf.baudrate = 57600;
 
-    u_int8_t res;
     AX_servo_init(conf);
     AX_servo_ping(conf, 1);
 }
